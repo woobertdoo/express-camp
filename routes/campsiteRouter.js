bfe1aa4fc633245d6a62080600a77e06de394b37
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2023-07-08 11:36:28 -0500

    Week 1 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -16,4 +16,17 @@ campsiteRouter.route('/').all((req, res, next) => {
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId').all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+}).get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);  
+}).put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+}).delete((req, res) => {
+    res.end(`Deteling campsite: ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +12,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,8 +22,8 @@ app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-})
+});
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
